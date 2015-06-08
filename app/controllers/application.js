e3f678832bcd35482cbef412fa1c622e66587abc
Thomas Poinsot <thomas.poinsot1@gmail.com>
2015-06-08 09:39:33 +0200

    Working sidebar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  artistID: "43ZHCT0cAZBISjO8DG9PnE",
-
-  actions: {
-    updateArtistID: function(name) {
-      Ember.$.get("https://api.spotify.com/v1/search?q=" + name + "&type=artist").then((data) => {
-        if(data.artists.items.length > 0) {
-          this.set("artistID", data.artists.items[0].id)
-        }
-      });
-    }
-  }
+  artistID: "43ZHCT0cAZBISjO8DG9PnE"
 });
