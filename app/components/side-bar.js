6f967482b576d89752e48b5d2dff46a0cea69a3b
Thomas Poinsot <thomas.poinsot1@gmail.com>
2015-06-09 17:11:55 +0200

    Added credits for thibault, and made them a bit better looking

--- app/components/side-bar.js
+++ app/components/side-bar.js
@@ -13,7 +13,7 @@ export default Ember.Component.extend({
     updateArtistID: function(name) {
       Ember.$.get("https://api.spotify.com/v1/search?q=" + name + "&type=artist").then((data) => {
         if(data.artists.items.length > 0) {
-          this.set("artistID", data.artists.items[0].id)
+          this.set("artistID", data.artists.items[0].id);
         }
       });
     },
