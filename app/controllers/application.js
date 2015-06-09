e9c339be45d1e18ebd5b91095a6c8de4276df0a7
Thomas Poinsot <thomas.poinsot1@gmail.com>
2015-06-09 10:43:06 +0200

    Now stores the selected artist as a query param

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  artistID: "43ZHCT0cAZBISjO8DG9PnE"
+  queryParams: ['artistID'],
+
+  artistID: "43ZHCT0cAZBISjO8DG9PnE",
+
+  artistNameUpdater: function() {
+    this.set('artistID', this.get('selectedNode.id'));
+  }.observes("selectedNode.id")
 });
